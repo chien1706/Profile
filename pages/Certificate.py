883fd17793f13650782676fbb09297d6968ba505
chien1706 <trinhvanchien1706@gmail.com>
2023-04-10 23:18:24 +0700

    new

--- pages/Certificate.py
+++ pages/Certificate.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
-image = Image.open("D:\Code\\upwork\\certi\\Algorithm_Toolbox.jpg")
-image2 = Image.open("D:\Code\\upwork\\certi\\SQL_DataScience.jpg")
-image3 = Image.open("D:\Code\\upwork\\certi\\Deep_learning(ImprovingDNN).jpg")
-image4 = Image.open("D:\Code\\upwork\\certi\\bootcamp_techainer.jpg")
+image = Image.open("D:\Code\\upwork\\multiple\\certi\\Algorithm_Toolbox.jpg")
+image2 = Image.open("D:\Code\\upwork\\multiple\\certi\\SQL_DataScience.jpg")
+image3 = Image.open("D:\Code\\upwork\\multiple\\certi\\Deep_learning(ImprovingDNN).jpg")
+image4 = Image.open("D:\Code\\upwork\\multiple\\certi\\bootcamp_techainer.jpg")
 
 # Show the image in Streamlit
 st.markdown('# 1.Algorithm Toolbox Certificate')
